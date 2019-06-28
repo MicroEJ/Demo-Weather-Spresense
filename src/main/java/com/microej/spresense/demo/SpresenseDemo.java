310ce6603e52558cc958058a2b5037151c3c8a52
Regis Caillaud <regis.caillaud@microej.com>
2019-06-28 18:11:58 +0200

    Added a first audio file

--- src/main/java/com/microej/spresense/demo/SpresenseDemo.java
+++ src/main/java/com/microej/spresense/demo/SpresenseDemo.java
@@ -13,6 +13,14 @@ import com.microej.spresense.demo.widget.MainFrame;
 
 import ej.animation.Animation;
 import ej.animation.Animator;
+import ej.audio.AudioFile;
+import ej.audio.AudioFile.AudioChannel;
+import ej.audio.AudioFile.AudioCodec;
+import ej.audio.AudioFile.BitLength;
+import ej.audio.AudioPlayer;
+import ej.audio.Playlist;
+import ej.bon.Timer;
+import ej.bon.TimerTask;
 import ej.components.dependencyinjection.ServiceLoaderFactory;
 import ej.microui.MicroUI;
 import ej.widget.StyledDesktop;
@@ -21,6 +29,10 @@ import ej.widget.StyledPanel;
 
 public class SpresenseDemo {
 	private static long INIT = 1557844987038L;
+	private static final int LOOP_INTERVAL = 10;
+	private static final String sunny = "AUDIO/sunny.mp3";
+	private static AudioPlayer audioPlayer;
+
 	public static void main(String[] args) {
 		if (System.currentTimeMillis() < INIT) {
 			ej.bon.Util.setCurrentTimeMillis(INIT);
@@ -42,6 +54,24 @@ public class SpresenseDemo {
 		panel.setWidget(new MainFrame());
 		desktop.show();
 		panel.showFullScreen(desktop);
+		LoopSound();
+	}
+
+	private static void LoopSound() {
+		audioPlayer = AudioPlayer.getInstance();
+		Playlist p = new Playlist();
+		p.addAudioFile(new AudioFile(sunny, AudioChannel.AS_CHANNEL_STEREO,BitLength.BITLENGTH_16, 44100 ,AudioCodec.MP3));
+
+		audioPlayer.initialize(p);
+		audioPlayer.launch();
+		Timer timer = new Timer();
+		TimerTask task = new TimerTask() {
+			@Override
+			public void run() {
+				audioPlayer.next(LOOP_INTERVAL);
+			}
+		};
+		timer.schedule(task,0, LOOP_INTERVAL*1000);
 	}
 
 	private SpresenseDemo() {
