65ac05cc76d730d3b358229c6203209a26e56956
Pierre Rivron <pierre.rivron@microej.com>
2019-10-11 09:59:12 +0200

    CC0213SONYSPRESENSE-64 Fix font usage

--- SpresenseDemo/src/main/java/com/microej/spresense/demo/style/FontProfiles.java
+++ SpresenseDemo/src/main/java/com/microej/spresense/demo/style/FontProfiles.java
@@ -18,7 +18,7 @@ public final class FontProfiles {
 
 
 	public static final FontProfile SMALL = new FontProfile(PIXEL, 15, Font.STYLE_PLAIN);
-	public static final FontProfile MEDIUM_BOLD = new FontProfile(ROBOTO, 22, Font.STYLE_BOLD);
+	public static final FontProfile MEDIUM_BOLD = new FontProfile(ROBOTO, 22, Font.STYLE_BOLD | Font.STYLE_ITALIC);
 	public static final FontProfile MEDIUM = new FontProfile(ROBOTO, 20, Font.STYLE_PLAIN);
 	public static final FontProfile HUGE = new FontProfile(SOURCE, 60, Font.STYLE_PLAIN);
 
