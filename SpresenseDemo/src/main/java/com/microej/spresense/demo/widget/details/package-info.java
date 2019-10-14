10fd19f72e82d5ef397ea2966ef6850cc4d2c7a0
Pierre Rivron <pierre.rivron@microej.com>
2019-10-14 12:05:51 +0200

    CC0213SONYSPRESENSE-64 Add package info

---
+++ SpresenseDemo/src/main/java/com/microej/spresense/demo/widget/details/package-info.java
@@ -0,0 +1,11 @@
+/*
+ * Java
+ *
+ * Copyright 2019 Sony Corp. All rights reserved.
+ * This Software has been designed by MicroEJ Corp and all rights have been transferred to Sony Corp.
+ * Sony Corp. has granted MicroEJ the right to sub-licensed this Software under the enclosed license terms.
+ */
+/**
+ * Widgets describing the sensors values.
+ */
+package com.microej.spresense.demo.widget.details;
